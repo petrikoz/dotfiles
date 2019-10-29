00376e39ab3b02be4413d2d4b8ff130d56ff3d7b
Petr Zelenin <po.zelenin@gmail.com>
2019-10-29 17:47:16 +0500

    [systemd/cloud-backup] add Nextcloud

--- systemd/user/cloud-backup/cloud-backup.sh
+++ systemd/user/cloud-backup/cloud-backup.sh
@@ -11,17 +11,25 @@ decrypted="$HOME/encfs/cloud"
 if [[ ! "$(findmnt -M $decrypted)" ]]; then
     echo "Please mount encrypted cloud's backup to '$decrypted'"
 else
-    echo "Backup private data:..."
+    echo "Backup private data: ..."
 
     # self config
-    echo "  rclone:..."
+    echo "  rclone: ..."
     "$rclone" copy "$HOME/.config/rclone" "$decrypted/rclone"
-    echo "  done"
+    echo "  rclone: done"
 
     # Google Drive
-    echo "  Google Drive:..."
+    echo "  Google Drive: ..."
     "$rclone" copy drive:Finances "$decrypted/fin-reports/drive.google.com"
-    echo "  done"
+    echo "  Google Drive: done"
+
+    # Nextcloud instance config
+    echo "  Nextcloud: ..."
+    nextcloud_remote="wormhole:/var/www/nextcloud"
+    nextcloud_local="$decrypted/nextcloud/"
+    rsync -az "$nextcloud_remote/config/config.php" "$nextcloud_local"
+    rsync -az "$nextcloud_remote/data/owncloud.db" "$nextcloud_local"
+    echo "  Nextcloud: done"
 fi
 
 
@@ -30,7 +38,7 @@ backup="$HOME/cloud/backup"
 storages=(/mnt/raid0/cloud dropbox:backup)
 echo "Sync storages:"
 for storage in "${storages[@]}"; do
-    echo "  '$storage':..."
+    echo "  '$storage': ..."
     "$rclone" sync "$backup" "$storage"
-    echo "  done"
+    echo "  '$storage': done"
 done
