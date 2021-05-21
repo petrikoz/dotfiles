2a0729677df5634c792ef2dddab62ecce1035e17
Petr Zelenin <po.zelenin@gmail.com>
2021-05-21 15:20:04 +0500

    [project] improve `sql/fix-local-data`

--- project/tmp/sql/fix-local-data.sql
+++ project/tmp/sql/fix-local-data.sql
@@ -2,3 +2,8 @@
 UPDATE auth_user
     SET password = 'pbkdf2_sha256$36000$mUvjGNuoKowK$6iG/ciPVfyrsNqNAZYlsS5HUD7k/OiZrG1QosbB+Up8='
     WHERE username = 'admin';
+
+-- set domain for localhost
+UPDATE django_site
+    SET domain = 'localhost:8000'
+    WHERE id = 1;
