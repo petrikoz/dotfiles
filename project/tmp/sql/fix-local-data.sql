b8dcf31b62d4d61208c2a24471eef62bc1f87b53
Petr Zelenin <po.zelenin@gmail.com>
2022-04-03 17:55:30 +0500

    [project] improvments
    * add `tmp/fixtures` for local Django's fixtures
    * add `tmp/nextjs-env.local` config for NextJS
    * add 'authtoken_token' INSERT in `fix-local-data.sql`

--- project/tmp/sql/fix-local-data.sql
+++ project/tmp/sql/fix-local-data.sql
@@ -31,6 +31,22 @@ DO UPDATE SET
 	is_staff=EXCLUDED.is_staff,
 	is_active=EXCLUDED.is_active;
 
+-- set superuser's authtoken:
+INSERT INTO
+	authtoken_token (
+		key,
+		created,
+		user_id
+	)
+VALUES (
+	'236cdd541215b122fd3415de0155664f7512221a',
+	now(),
+	(SELECT id FROM auth_user WHERE username = 'admin' LIMIT 1)
+) ON CONFLICT
+	ON CONSTRAINT authtoken_token_user_id_key
+DO UPDATE SET
+	key = EXCLUDED.key;
+
 -- set domain for localhost
 UPDATE django_site
 	SET domain = 'localhost:8000'
